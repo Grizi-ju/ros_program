64a0bdeb1545b00b9b4e209a139338cfd827c71a
xiaoju <80267952+Grizi-ju@users.noreply.github.com>
2022-02-11 21:33:34 +0800

    topic_01

---
+++ pub_vel.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from geometry_msgs.msg import Twist
+
+def talker():
+    pub = rospy.Publisher('/tianbot_mini/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    
+    while not rospy.is_shutdown(): 
+        vel_cmd = Twist()
+        vel_cmd.linear.x = 1.0
+        vel_cmd.angular.z = 1.0
+           
+        pub.publish(vel_cmd)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
