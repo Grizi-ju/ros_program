a27199bd8a657c33139f919de2f0ec8958e970cf
xiaoju <80267952+Grizi-ju@users.noreply.github.com>
2022-02-12 21:38:42 +0800

    service_01

---
+++ turtle_spawn_client.cpp
@@ -0,0 +1,35 @@
+/**
+ * 服务通信：该例程在turtlesim仿真环境下，请求/spawn服务，服务数据类型turtlesim::Spawn
+ */
+
+#include <ros/ros.h>
+#include <turtlesim/Spawn.h>
+
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "turtle_spawn");
+	ros::NodeHandle n;
+
+    // 发现/spawn服务后，创建一个服务客户端，连接名为/spawn的service
+	ros::service::waitForService("/spawn");
+	ros::ServiceClient add_turtle_client = n.serviceClient<turtlesim::Spawn>("/spawn");
+
+    // 初始化turtlesim::Spawn的请求数据
+	turtlesim::Spawn srv;
+	srv.request.x = 2.0;
+	srv.request.y = 2.0;
+	srv.request.name = "turtle2";
+
+    // 请求服务调用
+	ROS_INFO("Call service to spawn turtle[x:%0.6f, y:%0.6f, name:%s]", 
+			 srv.request.x, srv.request.y, srv.request.name.c_str());
+
+	add_turtle_client.call(srv);
+
+	// 显示服务调用结果
+	ROS_INFO("Spwan turtle successfully [name:%s]", srv.response.name.c_str());
+
+	return 0;
+};
+
